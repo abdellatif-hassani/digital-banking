cb5bfc45ce24f738f798e467a56f50ab8e188645
SasboHassani <abdellatifhassani1205@gmail.com>
2024-05-09 20:18:52 +0100

    first commit

---
+++ back-end/src/test/java/hassani/abdellatif/backend/BackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package hassani.abdellatif.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
